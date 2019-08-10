a826bf1234990e5187c8c74c598f48c029fd27ed
Jêrome Rx <jerome+git@rouaix.eu>
2019-08-10 14:40:01 +0200

    First benchmark : Rust is twice+ worst

---
+++ csharp-expr-rs.Benchmarks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace csharp_expr_rs.Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<TinyExpressionBenchmark>();
+        }
+    }
+}
---
+++ csharp-expr-rs.Benchmarks/TinyExpressionBenchmark.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using DynamicExpresso;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace csharp_expr_rs.Benchmarks
+{
+    public class TinyExpressionBenchmark : IDisposable
+    {
+        private Lambda _dynamicExpression;
+        private Expression _rustExpression;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            // DynamicExpresso
+            var interpreter = new Interpreter(InterpreterOptions.DefaultCaseInsensitive);
+            interpreter.SetFunction("first", (Func<object, int, int, object>)((a, b, c) => new[] { a, b, c }.First()));
+
+            _dynamicExpression = interpreter.Parse("first(1,2,3)");
+
+            //Rust
+            _rustExpression = new Expression("first(1,2,3)");
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            _rustExpression.Dispose();
+        }
+
+        [Benchmark(Baseline = true)]
+        public object DynamicExpresso() => _dynamicExpression.Invoke();
+
+        [Benchmark]
+        public object Rust() => _rustExpression.Execute();
+    }
+}
